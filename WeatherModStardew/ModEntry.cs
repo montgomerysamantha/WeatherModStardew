89f0415d839565a3fabac72c3d9130f6796d2e8b
montgomerysamantha <montgomerysamantha5@gmail.com>
2020-04-28 20:55:28 -0500

    Outputs tomorrow's weather to the console

--- WeatherModStardew/ModEntry.cs
+++ WeatherModStardew/ModEntry.cs
@@ -4,6 +4,7 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.Objects;
 
 namespace WeatherModStardew
 {
@@ -17,7 +18,9 @@ namespace WeatherModStardew
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            //helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+            helper.Events.GameLoop.DayStarted += (s, e) => OnDayStarted();
+            //helper.Events.GameLoop.DayStarted += this.OnDayStarted;
         }
 
 
@@ -34,7 +37,21 @@ namespace WeatherModStardew
                 return;
 
             // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+            //this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+        }
+
+        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event data.</param>
+        private void OnDayStarted()
+        {
+            // ignore if player hasn't loaded a save yet
+            if (!Context.IsWorldReady)
+                return;
+
+            string forecast = this.Helper.Reflection.GetMethod(new TV(), "getWeatherForecast").Invoke<string>();
+
+            this.Monitor.Log($"{forecast}, tomorrow", LogLevel.Debug);
         }
     }
 }
